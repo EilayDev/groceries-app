198b6dbd5c788d32e19f54cb84ed9a9d2c217c07
BangBuster <eliayeliay2001@gmail.com>
2021-04-21 09:09:42 +0200

    ShoppingLists changes

--- Components/ShoppingLists.jsx
+++ Components/ShoppingLists.jsx
@@ -1,11 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect, preventDefault } from 'react';
 import AddCircleOutlineRoundedIcon from '@material-ui/icons/AddCircleOutlineRounded';
 import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import {Tooltip} from '@material-ui/core';
+import { Tooltip } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
+import ButtonBase from '@material-ui/core/ButtonBase';
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -21,68 +23,93 @@ const useStyles = makeStyles((theme) => ({
     '&:hover': {
       'cursor': 'pointer !important'
     },
-    
+
   },
   ListField: {
-    width:'100%',
-    padding:'0'
+    width: '100%',
+    padding: '0'
   },
   fullHeight: {
-    height:'100%'
+    height: '100%'
   },
 }));
 
-
+const placeholdLists = [
+  {
+    label: "Default",
+    id: 0
+  },
+  {
+    label: "Test2",
+    id: 1
+  }
+];
 
 export default function ShoppingLists() {
   const classes = useStyles();
   const [selectedTab, setSelectedTab] = useState(0);
-  const [lists, setLists] = useState([]);
+  const [lists, setLists] = useState(placeholdLists);
 
-  const handleChange = (event, newValue) => {
+  const handleChange = (_event, newValue) => {
     setSelectedTab(newValue);
   };
 
-  const placeholdLists = [
-    {
-      label: "Test1",
-    },
-    {
-      label: "Test2"
-    }
-  ];
-  const handleAdd = (event) => {
+
+  const handleAdd = (_event) => {
     setLists(prevData => {
-      return [...prevData, {label: "new"}]
+      return [...prevData, { label: "new", id: prevData.length }]
     })
   }
+
+  const handleDelList = (event) => {
+    event.stopPropagation();
+    if(lists.length==1){return}
+    setLists((prevData) => (
+      prevData.filter((_e, i) => i != selectedTab)))
+    if (selectedTab == lists.length - 1) {
+      let tabs = document.getElementsByClassName("MuiTab-textColorInherit");
+      tabs[tabs.length - 2].click();
+    }
+  }
+
+
+
+  const CustomAdd = () => {
+    return (
+      <>
+        <Tooltip title="Add a list">
+          <AddCircleOutlineRoundedIcon className={classes.addIcon} onClick={handleAdd} />
+        </Tooltip>
+        <Divider />
+      </>
+    )
+  }
+
+  const TabWrapper = React.forwardRef((props, ref) => {
+    return (
+      <ButtonBase ref={ref} {...props}> {props.children} {props.className.includes("Mui-selected") && <CloseIcon onClick={handleDelList} />} </ButtonBase >
+    )
+  })
+
 
   return (
     <div className={`${classes.selected} ${classes.fullHeight}`}>
       <Tabs
         orientation="vertical"
-        variant="scrollable"
         value={selectedTab}
         onChange={handleChange}
-        >
-        {placeholdLists.map((list, index) => (
-          <Tab key={index} label={list.label}/>
-        ))}
-        
-        
+      >
         {lists.map((list, index) => (
-            <Tab key={index} label={list.label}/>
+          <Tab key={index} label={list.label} component={TabWrapper} />
         ))}
-            
+        <CustomAdd />
       </Tabs>
-      <Tooltip title="Add a list">
-        <AddCircleOutlineRoundedIcon className={classes.addIcon} onClick={handleAdd}/>
-      </Tooltip>
-      <Divider/>
-      
-       
-      </div>
-      
-      
+
+
+
+
+    </div>
+
+
   );
 }
