54a03fd0ce3c3986051ab5279da9f2bd792a15dc
BangBuster <eliayeliay2001@gmail.com>
2021-04-28 12:00:34 +0200

    responsive drawer button

--- Components/Header/Header.jsx
+++ Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, IconButton, useMediaQuery } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import { useDispatch } from 'react-redux'
 import {openDrawer} from '../../redux/drawer/drawerReducer'
@@ -6,8 +6,13 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     white: ({
-        color: 'white'
+        color: 'white',
     }),
+    smdisplay: {
+        [theme.breakpoints.up('sm')]: {
+            'display': 'none' 
+        }
+    },
     zindex: {
         'z-index': theme.zIndex.drawer + 1,
         'position': 'relative'
@@ -23,7 +28,7 @@ export default function Header() {
     return(
         <AppBar position="static" className={`${classes.appBar} ${classes.zindex}`}>
             <Toolbar>
-            <IconButton onClick={handleDrawer} edge="start" className={classes.white} aria-label="menu">
+            <IconButton onClick={handleDrawer} edge="start" className={`${classes.white} ${classes.smdisplay}`} aria-label="menu">
                 <MenuIcon />
             </IconButton>
             <Typography variant="h6" className={classes.white}>
