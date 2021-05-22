dcdcfef3f5948d7a6129f56fba9c113063e403cb
BangBuster <eliayeliay2001@gmail.com>
2021-05-22 16:09:28 +0200

    frontend for room creation

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import {Card, CardContent, Grid, Button, Divider, TextField} from '@material-ui/core';
+import {Card, CardContent, Grid, Button, Divider, TextField, Modal, Link} from '@material-ui/core';
+import DoneAllRoundedIcon from '@material-ui/icons/DoneAllRounded';
 import { useRouter } from 'next/router'
 
-export default function Login(props){
-    const router = useRouter()
+const useStyles = makeStyles((theme) => ({
+    centerModal:{
+        'top': '50%',
+        'left': '50%',
+        'transform': 'translate(-50%, -50%)',
+    }
+}))
 
-    async function createRoom() {
-        await fetch('/api/createRoom')
+export default function Login(props){
+    const classes = useStyles();
+    const router = useRouter()
+    const [open, setOpen] = useState(false);
+    const [ROOMID, setROOMID] = useState();
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    function createRoom() {
+        fetch('/api/createRoom')
             .then(response=>response.json())
             .then(data=>{
-                router.push('/rooms/'+data.roomID)
+                //router.push('/rooms/'+data.roomID)
+                setROOMID(data.roomID)
+                handleOpen()
             })
     }
     return (
+        <>
         <Grid container justify="center" alignItems="center" style={{height:'100%'}}>
             <Card style={{marginBottom:'10%'}}>
+                {/* TODO: implement join existing room */}
                 <CardContent style={{textAlign:"center"}}>
                     Login page!
                     <br/>
@@ -41,5 +64,27 @@ export default function Login(props){
                 </CardContent>
             </Card>
         </Grid>
+        <Modal
+            open={open}
+            onClose={handleClose}
+        >
+            <Grid container justify="center" alignItems="center" style={{height:'100%'}}>
+                <Card>
+                    <CardContent>
+                        Your new room has been created <span><DoneAllRoundedIcon fontSize="small" style={{ color: 'rgb(37 141 41)', 'verticalAlign': 'bottom' }}/></span>
+                        <br/>
+                        Room's URL:
+                        <br/>
+                        <Link href={'/rooms/' + ROOMID} color="initial">
+                            {process.browser &&
+                                window.location.href + 'rooms/' + ROOMID
+                            }
+                            
+                        </Link>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Modal>
+        </>
     )
 }
